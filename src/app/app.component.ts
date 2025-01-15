fb8c297bf4e91252003272788118ec793706da9e
xamaztian <sebastian.gonzalez.bugueno@gmail.com>
2025-01-15 16:19:59 -0300

    Cambios para button y visibilidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: string = 'Hola Mundial Angular!';
+  title: string = 'Hola Mundial Angular! desde componente';
 
-  users: string[] = ['Juan', 'María', 'Luis'];
+  users: string[] = ['Juan', 'María', 'Luis', 'Pepe'];
+
+  visible : boolean = false;
+
+  setVisible() : void{
+    this.visible = !this.visible;
+    console.log("Hicimos Click en Set visible");
+  }
+
 }
