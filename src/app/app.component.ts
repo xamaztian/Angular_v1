f8ae4ddc42ed482de68b5d6e3a70bfbe854c6f87
xamaztian <sebastian.gonzalez.bugueno@gmail.com>
2025-01-15 15:36:27 -0300

    Cambios para ejercicio, corrección de librerías ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title: string = 'Hola Mundial Angular!';
+
+  users: string[] = ['Juan', 'María', 'Luis'];
 }
